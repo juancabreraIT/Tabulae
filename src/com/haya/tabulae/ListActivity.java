541da7b9750e929d69d676d266eae11b498afa11
jlcr <jlcr@PCMAD228.gft.com>
2015-09-28 13:46:28 +0200

    [LIST] Mock activity

--- src/com/haya/tabulae/ListActivity.java
+++ src/com/haya/tabulae/ListActivity.java
@@ -1,16 +1,33 @@
 package com.haya.tabulae;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class ListActivity extends Activity {
-
+	
+	ListView list;
+	Spinner marketSelector;
+	TextView price;
+	
+	// Mock
+	ArrayList<String> items = new ArrayList<String>();
+	ArrayList<String> markets = new ArrayList<String>();
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_list);
+		
+		init();
+		mock();
 	}
 
 	@Override
@@ -30,5 +47,37 @@ public class ListActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+
+	
+	private void init() {
+		
+		list = (ListView) findViewById(android.R.id.list);
+		marketSelector = (Spinner) findViewById(R.id.marketSelector);
+		price = (TextView) findViewById(R.id.price);
+			
+	}
+	
+	private void mock() {
+		
+		// LISTVIEW
+		items.add("Tomate");
+		items.add("Pan");
+		items.add("Detergente");
+		
+		ArrayAdapter<String> adapterList = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+		list.setAdapter(adapterList);
+		
+		// SPINNER
+		markets.add("Mercadona");
+		markets.add("Aldi");
+		markets.add("Super Sol");
+
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, markets);				
+		marketSelector.setAdapter(adapter);
+		
+		// TEXTVIEW
+		price.setText("23.42€");
+		
 	}
 }
