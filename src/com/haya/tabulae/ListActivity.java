3e473448e6e60da1230c5f34ef650795091e9640
jlcr <jlcr@PCMAD228.gft.com>
2015-09-28 16:21:19 +0200

    [LISTA] Minor fixex & adjust layout.

--- src/com/haya/tabulae/ListActivity.java
+++ src/com/haya/tabulae/ListActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.view.WindowManager.LayoutParams;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -64,13 +65,12 @@ public class ListActivity extends Activity {
 		
 		list = (ListView) findViewById(android.R.id.list);
 		marketSelector = (Spinner) findViewById(R.id.marketSelector);
-		price = (TextView) findViewById(R.id.price);
+		price = (TextView) findViewById(R.id.price);		
 		
 		list.setOnItemClickListener(new OnItemClickListener() {
 
 			@Override
 			public void onItemClick(AdapterView<?> arg0, View arg1, int position, long id) {
-				Log.d("Tabulae", "Deleting element " + position);
 				Toast.makeText(getApplicationContext(), items.get(position) + " picked!", Toast.LENGTH_SHORT).show();
 				items.remove(position);
 				adapterList.notifyDataSetChanged();
